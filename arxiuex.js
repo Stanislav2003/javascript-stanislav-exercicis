347227b5298b23cd2cfdcf00e3d3fb573c21ea78
Stanislav2003 <94047550+Stanislav2003@users.noreply.github.com>
2021-11-22 21:55:03 +0100

    Add files via upload

---
+++ arxiuex.js
@@ -0,0 +1,51 @@
+
+
+/*EX6
+const numero_bigint = 5678n
+const myNumber = Number(numero_bigint)
+const numero_number = 1234
+
+console.log(myNumber*numero_number)
+console.log(myNumber+numero_number)
+console.log(myNumber-numero_number)
+console.log(myNumber/numero_number)*/
+
+/* ex 7
+let catet1 = 4
+let catet2 = 4
+console.log(Math.hypot(catet1,catet2))*/
+
+/* ex 8
+let numero1 = 10
+let texto = 'hola mon'
+console.log(numero1/texto)*/
+
+/* ex 9 
+let nomCognoms = 'Stanislav Sevdalinov Ivanov'
+console.log(nomCognoms.slice(10,20).length);
+console.log(nomCognoms.slice(10,20))*/
+
+/* ex10 
+let nomCognoms = 'Stanislav Sevdalinov Ivanov'
+console.log(nomCognoms.replace('sl','l'))*/
+
+/* ex11
+let variable1
+let var2=null
+console.log(variable1+var2)*/
+
+/*ex12
+console.log(String(true))
+console.log(String+true)*/
+
+/*ex13
+console.log(Boolean('bon dia a tots!'))
+console.log(true || 'bones again');*/
+
+
+console.log(Number(true))
+console.log(33 == true)
+
+
+
+
